90106f0df4b2f55f1a62f0d143ecc2321a10f5e8
Pieter <44448043+PieterDePauw@users.noreply.github.com>
2023-09-26 04:06:38 +0200

    🛠️refactor(utils) add findObjectInState to exports

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -6,8 +6,9 @@ import getApiUrl from "./getUrl";
 import replaceTempId from "./replacetempid";
 import getResourceTypePlural from "./convertplural";
 import findObject from "./findObject";
+import findObjectInState from "./findobjectinstate";
 
 import { TODOIST_AUTOCOMMIT, TODOIST_BASE_URL, TODOIST_RESOURCE_TYPES } from "./env";
 
 
-export { deepcopy, deepmerge, validateToken, capitalizeFirstLetter, getApiUrl, replaceTempId, getResourceTypePlural, findObject, TODOIST_AUTOCOMMIT, TODOIST_BASE_URL, TODOIST_RESOURCE_TYPES };
+export { deepcopy, deepmerge, validateToken, capitalizeFirstLetter, getApiUrl, replaceTempId, getResourceTypePlural, findObject, findObjectInState, TODOIST_AUTOCOMMIT, TODOIST_BASE_URL, TODOIST_RESOURCE_TYPES };
