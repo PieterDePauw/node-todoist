be159851cc589d8f908ff1ba102a94e950d164b8
Pieter <44448043+PieterDePauw@users.noreply.github.com>
2023-09-26 03:01:44 +0200

    ⚙️chore(utils) update index.ts
    Reason: Added the "findObject" function to the export list in the "index.ts" file in the "utils" scope.

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,9 +4,10 @@ import validateToken from "./validatetoken";
 import capitalizeFirstLetter from "./capitalizefirstletter";
 import getApiUrl from "./getUrl";
 import replaceTempId from "./replacetempid";
-import { getResourceTypePlural } from "./convertplural";
+import getResourceTypePlural from "./convertplural";
+import findObject from "./findObject";
 
 import { TODOIST_AUTOCOMMIT, TODOIST_BASE_URL, TODOIST_RESOURCE_TYPES } from "./env";
 
 
-export { deepcopy, deepmerge, validateToken, capitalizeFirstLetter, getApiUrl, replaceTempId, getResourceTypePlural, TODOIST_AUTOCOMMIT, TODOIST_BASE_URL, TODOIST_RESOURCE_TYPES };
+export { deepcopy, deepmerge, validateToken, capitalizeFirstLetter, getApiUrl, replaceTempId, getResourceTypePlural, findObject, TODOIST_AUTOCOMMIT, TODOIST_BASE_URL, TODOIST_RESOURCE_TYPES };
