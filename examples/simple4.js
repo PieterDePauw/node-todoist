e57af60c8d1e74ec18f723b213323b68423e41d0
Pieter <44448043+PieterDePauw@users.noreply.github.com>
2023-09-26 07:05:53 +0200

    🚀feat(sync) add delay time for sync and improve logging 🚀feat(sync) commit changes after syncing 🚀feat(items) add delay time before adding new item 🛠️refactor(items) rename variable for added item 🚀feat(sync) commit changes after adding new item ⚙️chore(sync) add delay time before getting items ⚙️chore(sync) commit changes after getting items 🛠️refactor(items) rename variable for fetched items ⚙️chore(sync) add delay time before closing item ⚙️chore(sync) commit changes after closing item ⚙️chore(sync) add delay time before getting items again ⚙️chore(sync) commit changes after getting items again

--- examples/simple4.js
+++ examples/simple4.js
@@ -32,35 +32,37 @@ async function delay(seconds) {
   console.log("SYNC TOKEN HAS BEEN SETTED TO:", returnedToken);
   console.log("----------------------------------");
 
-  //+ DELAY OF 2 SECONDS
-  await delay(2);
+  //+ DELAY OF 5 SECONDS
+  await delay(5);
 
   //! SYNC
   // Use api.sync() to get the latest state from the server
-  await api.sync()
-  console.log("SYNC");
+  await api.commit()
+  console.log("SYNC – COMMIT");
   console.log("----------------------------------");
 
   //! GET ITEMS A
   // Use api.items.get() to get all items from the state
   console.log("GET ITEMS A");
   const itemsA = api.items.get()
-  const itemsASorted = itemsA.map((i, index) => [index, i.id, i.content])
+  const itemsASorted = itemsA.map((i, index) => [index, i.id, i.content, i.checked])
   console.log(itemsASorted)
   console.log("----------------------------------");
 
-  //+ DELAY OF 2 SECONDS
-  await delay(2);
+  //+ DELAY OF 5 SECONDS
+  await delay(5);
 
   //! ADD NEW ITEM 1
   // Use api.items.add() to add a new item
-  const newItem = await api.items.add({ content: 'COMPLETED ITEM!' })
+  const itemToAdd = { content: 'COMPLETED ITEM!' }
+  const content = itemToAdd.content
+  const newItem = await api.items.add(itemToAdd)
   console.log("ADD NEW ITEM 1");
   console.log(newItem);
   console.log("----------------------------------");
 
-  //+ DELAY OF 2 SECONDS
-  await delay(2);
+  //+ DELAY OF 5 SECONDS
+  await delay(5);
 
   //+ COMMIT
   // Use api.commit() to commit the changes
@@ -68,19 +70,28 @@ async function delay(seconds) {
   console.log("COMMIT");
   console.log("----------------------------------");
 
-  //+ DELAY OF 2 SECONDS
-  await delay(2);
+  //+ DELAY OF 5 SECONDS
+  await delay(5);
+
+  //+ SYNC
+  // Use api.sync() to get the latest state from the server
+  await api.commit()
+  console.log("SYNC – COMMIT");
+  console.log("----------------------------------");
+
+  //+ DELAY OF 5 SECONDS
+  await delay(5);
 
   //! GET ITEMS B
   // Use api.items.get() to get all items from the state
   console.log("GET ITEMS B");
   const itemsB = api.items.get()
-  const itemsBSorted = itemsB.map((i, index) => [index, i.id, i.content])
+  const itemsBSorted = itemsB.map((i, index) => [index, i.id, i.content, i.checked])
   console.log(itemsBSorted)
   console.log("----------------------------------");
 
-  //+ DELAY OF 2 SECONDS
-  await delay(2);
+  //+ DELAY OF 5 SECONDS
+  await delay(5);
 
   //+ COMMIT
   // Use api.commit() to commit the changes
@@ -88,21 +99,35 @@ async function delay(seconds) {
   console.log("COMMIT");
   console.log("----------------------------------");
 
+  //+ DELAY OF 5 SECONDS
+  await delay(5);
+
+  //! GET ITEMS B2
+  // Use api.items.get() to get all items from the state
+  console.log("GET ITEMS B2");
+  const itemsB2 = api.items.get()
+  const itemsB2Sorted = itemsB.map((i, index) => [index, i.id, i.content, i.checked])
+  console.log(itemsB2Sorted)
+  console.log("----------------------------------");
+
+  //+ DELAY OF 5 SECONDS
+  await delay(5);
+
   //! Get item by content string
-  const itemToClose = itemsB.find((item) => item.content === "COMPLETED ITEM!")
+  const itemToClose = (itemsB2).find(item => item["content"] === content)
   console.log("ITEM TO CLOSE", itemToClose);
-  const itemToCloseId = itemToClose.id
+  const itemToCloseId = itemToClose["id"]
   console.log(itemToCloseId);
 
   //! CLOSE ITEM
   // Use api.items.close() to close an item
-  const closedItem = await api.items.complete({ id: itemToCloseId})
+  const closedItem = await api.items.close({ id: itemToCloseId})
   console.log("CLOSE ITEM");
   console.log(closedItem);
   console.log("----------------------------------");
 
-  //+ DELAY OF 2 SECONDS
-  await delay(2);
+  //+ DELAY OF 5 SECONDS
+  await delay(5);
 
   //+ COMMIT
   // Use api.commit() to commit the changes
@@ -110,18 +135,26 @@ async function delay(seconds) {
   console.log("COMMIT");
   console.log("----------------------------------");
 
-  //+ DELAY OF 2 SECONDS
-  await delay(2);
+  //+ DELAY OF 5 SECONDS
+  await delay(5);
 
   //! GET ITEMS C
   // Use api.items.get() to get all items from the state
   console.log("GET ITEMS C");
   const itemsC = api.items.get()
-  const itemsCSorted = itemsC.map((i, index) => [index, i.id, i.content])
+  const itemsCSorted = itemsC.map((i, index) => [index, i.id, i.content, i.checked])
   console.log(itemsCSorted)
   console.log("----------------------------------");
 
-  //+ DELAY OF 2 SECONDS
-  await delay(2);
+  //+ DELAY OF 5 SECONDS
+  await delay(5);
+
+  //! GET ITEMS D
+  // Use api.items.get() to get all items from the state
+  console.log("GET ITEMS D");
+  const itemsD = api.items.get()
+  const itemsDSorted = itemsD.map((i, index) => [index, i.id, i.content, i.checked])
+  console.log(itemsDSorted)
+  console.log("----------------------------------");
   
 })()
